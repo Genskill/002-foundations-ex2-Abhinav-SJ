d388713b524a9e481606c705b6b2f322fd4eb779
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2021-05-20 20:54:29 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
-#include<math.h>
 
 int main(void)
 {
 	float a = get_float("side 1\n");
 	float b = get_float("side 2\n");
 	float c = get_float("side 3\n");
-	if((pow(a,2) + pow(b,2) == pow(c,2)) || (pow(b,2) + pow(c,2) == pow(a,2)) ||(pow(a,2) + pow(c,2) == pow(b,2)))
+	if((a*a + b*b == c*c) || (b*b + c*c == a*a) ||a*a + c*c == b*b)
 		printf("Yes");
 	else
 		printf("No");
